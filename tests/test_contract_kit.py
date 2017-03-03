e5b3bd29ee8add1fe848ebb48ac5513806e6364a
Raimon Esteve <raimon@nan-tic.com>
2017-03-03 17:17:15 +0100

    Add test

--- tests/test_contract_kit.py
+++ tests/test_contract_kit.py
@@ -1,12 +1,12 @@
 # This file is part of the contract_kit module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_checker
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+import unittest
 import doctest
 import trytond.tests.test_tryton
-import unittest
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import (doctest_setup, doctest_teardown,
+    doctest_checker)
 
 
 class ContractKitTestCase(ModuleTestCase):
@@ -18,9 +18,8 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ContractKitTestCase))
-    suite.addTests(doctest.DocFileSuite(
-            'scenario_explode_contract_kit.rst',
+    suite.addTests(doctest.DocFileSuite('scenario_contract_kit.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            checker=doctest_checker,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
